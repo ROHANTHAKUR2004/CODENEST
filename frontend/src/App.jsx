363079c5a62069fdc1168f4640a4503793f30629
rohan <rohanthakur89768@gmail.com>
2024-12-25 19:35:51 +0530

    connect backend in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
+import usePing from "./hooks/apis/query/usePing";
+
 function App() {
+
+
+   const {isLoading, data} =   usePing();
+
+   if(isLoading){
+    return (
+      <>
+       loading...
+      </>
+    )
+   }
   return (
     <>
-     hellollllll
+     hellollllll {data.message}
+    
     </>
   )
 }
